9ec8617ee43ea40e5c9299318de641ab2ecd1251
LAPTOP-SM63HCM6\sarah <sarahsteenhuis@live.nl>
2026-03-16 12:09:25 +0100

    fix vite base for dev

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === "development" ? "/" : "/bieskes-compendium-app/", //neccessary for github pages but breaks dev
+  files: ['**/*.{ts,tsx}'],
   plugins: [react()],
-})
+}))
