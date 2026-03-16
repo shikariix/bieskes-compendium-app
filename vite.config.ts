3ebaa5d6e3cb5c24f3d4b6b2405aa12c8e324304
LAPTOP-SM63HCM6\sarah <sarahsteenhuis@live.nl>
2026-03-16 12:48:11 +0100

    remove unwanted line of code from vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   base: "/bieskes-compendium-app/",
-  files: ['**/*.{ts,tsx}'],
   plugins: [react()],
 })
